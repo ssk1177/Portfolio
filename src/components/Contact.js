ed04dc29bdab901ffb4a8cf66292b7d0d2e698a2
surendra <surendra.rules@gmail.com>
2024-09-24 00:32:51 -0500

    form submit fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,62 +11,39 @@ const Contact = () => {
         </h1>
 
         {/* Form */}
-        <form name="contact" method="POST" data-netlify="true">
+        <form name="contact" netlify>
           {/* Hidden input for Netlify form handling */}
           <input type="hidden" name="form-name" value="contact" />
           <div className="form-row">
             <div className="form-group">
               <label htmlFor="firstName">First Name *</label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                required
-              />
+              <input type="text" id="firstName" name="firstName" required />
             </div>
             &nbsp;
             <div className="form-group">
               <label htmlFor="lastName">Last Name *</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                required
-              />
+              <input type="text" id="lastName" name="lastName" required />
             </div>
           </div>
 
           <div className="form-row">
             <div className="form-group">
               <label htmlFor="email">Email *</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-              />
+              <input type="email" id="email" name="email" required />
             </div>
           </div>
 
           <div className="form-row">
             <div className="form-group">
               <label htmlFor="subject">Subject</label>
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-              />
+              <input type="text" id="subject" name="subject" />
             </div>
           </div>
 
           <div className="form-row">
             <div className="form-group">
               <label htmlFor="message">Message</label>
-              <textarea
-                id="message"
-                name="message"
-                rows="5"
-              />
+              <textarea id="message" name="message" rows="5" />
             </div>
           </div>
 
